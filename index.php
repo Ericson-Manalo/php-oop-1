ec4d85aa9dbf1735dfa46d02bccda5c0b198565d
Ericson-Manalo <ericmana98@gmail.com>
2023-07-13 15:46:06 +0200

    added the function construct into my class Movie

--- index.php
+++ index.php
@@ -8,17 +8,24 @@ class Movie {
     public $language;
     //actor
 
-   // function __construct ( $_title, $_genre, $_year, $_language){
-      //  $this->title = $_title;
-        //$this->genre = $genre;
+    function _construct ( $_title, $_genre, $_year, $_language){
+        $this->title = $_title;
+        $this->genre = $genre;
+        $this->year = $year;
+        $this->language = $language;
 
-   // }
+    }
+
+
 
 }
 
 $missione_impossible = new Movie ();
 $missione_impossible -> title = 'Mission Impossible';
 $missione_impossible -> genre = 'action';
+$missione_impossible -> year = 1996;
+$missione_impossible -> language = 'English';
+
 var_dump ($missione_impossible);
 
 ?>
