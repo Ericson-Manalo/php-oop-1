af955c00219428c924f8d81b7694a6afcfd2d8b7
Ericson-Manalo <ericmana98@gmail.com>
2023-07-13 16:42:32 +0200

    created a class called actor in which i put properties as name surname and called it in the class Movie

--- index.php
+++ index.php
@@ -12,15 +12,15 @@ class Movie {
         String $_title, 
         String $_genre, 
         Int $_year, 
-        String $_language){
-
+        String $_language,
+        $_actor
+        ){
         $this->title = $_title;
         $this->genre = $_genre;
         $this->year = $_year;
         $this->language = $_language;
-
+        $this->actor = $_actor;
     }
-
 
     public function getMovieName(){
         return $this->title;
@@ -30,19 +30,22 @@ class Movie {
 }
 
 
-class Actor{
+class MainActor{
     public $name;
     public $surname;
     public $age;
     public $internationality;
 
-    function _construct(
+    function __construct(
         String $name,
         String $surname,
         Int $age,
         String $internationality
     ){
-
+    $this->name = $name;
+    $this->surname = $surname;
+    $this->age = $age;
+    $this->internationality = $internationality;
     }
 }
 
@@ -51,16 +54,19 @@ class Actor{
 //$mission_impossible -> genre = 'action';
 //$mission_impossible -> year = 1996;
 //$mission_impossible -> language = 'English';
-$mission_impossible = new Movie ('Mission Impossible', 'Action', 1996, 'English');
+$newActor = new MainActor ('Tom', 'Cruise', 1962, 'American');
+$mission_impossible = new Movie ('Mission Impossible', 'Action', 1996, 'English', $newActor);
 echo $mission_impossible -> getMovieName();
 var_dump ($mission_impossible);
 
 
-$insidious = new Movie ();
-$insidious -> title = 'Insidious: The Red Door';
-$insidious -> genre = 'horror';
-$insidious -> year = 2023;
-$insidious -> language = 'English';
+//$insidious = new Movie ();
+//$insidious -> title = 'Insidious: The Red Door';
+//$insidious -> genre = 'horror';
+//$insidious -> year = 2023;
+//$insidious -> language = 'English';
+$newActor = new MainActor ('Patrick', 'Wilson', 1973, 'American');
+$insidious = new Movie ('Insidious: The Red Door', 'horror', 2023, 'English', $newActor);
 echo $insidious -> getMovieName();
 var_dump ($insidious);
 
